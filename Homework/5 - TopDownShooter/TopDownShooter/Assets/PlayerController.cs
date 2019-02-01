0f70c24293af84aa49fd349d6b3a7b3b72efc593
Angel Lim <brian.q.lim@gmail.com>
2019-01-31 22:42:52 -0800

    I made a Shrek game
    why

---
+++ Homework/5 - TopDownShooter/TopDownShooter/Assets/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿//everything associated with player control
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10.0f;
+    //goal - instantiate a projectile at the placement of each of these weapons whenever a player presses a key
+    public GameObject[] weaponsPlacements;
+    public GameObject laserPrefab = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey (KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * speed);
+        }
+     
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            foreach (GameObject weaponPlacement in weaponsPlacements)
+            {
+                Instantiate(laserPrefab, weaponPlacement.transform.position, weaponPlacement.transform.rotation);
+            }
+        }
+    }//end of void update()         
+}//end of PlayerController
