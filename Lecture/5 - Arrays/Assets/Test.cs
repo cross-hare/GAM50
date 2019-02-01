0ff6815ca261bda016ce7227d95835252b6a7669
Angel Lim <brian.q.lim@gmail.com>
2019-01-31 20:56:36 -0800

    Week 5 Lecture on Arrays
    Nothing special

---
+++ Lecture/5 - Arrays/Assets/Test.cs
@@ -0,0 +1,25 @@
+﻿//arrays are good for storing a bunch of elements in a singular variable
+//arrays can be any data type, including data types you yourself create (such as GameObjects)
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public string[] screenNames;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //one of the problems with arrays is ambiguity. You have one variable that potentially stores a bunch of different values.
+        //print(screenNames) would not work.
+        //screenNames[1] would print the first index/element of the screenNames array
+        print(screenNames[1]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
