94a5a170a88ed60b69f79c997d0ac399333315df
Angel Lim <brian.q.lim@gmail.com>
2019-02-02 07:30:36 -0800

    FIXED huge enemy bug
    this one was so annoying

---
+++ Homework/5 - TopDownShooter/TopDownShooter/Assets/EnemySpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyPrefab = null;
+    public GameObject[] enemySpawnPoint;
+    public float spawnDelay = 3.0f;
+    public float timer = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= spawnDelay)
+        {
+            Instantiate(enemyPrefab, transform.position, transform.rotation);
+            timer = 0.0f;
+        }
+    }
+}
