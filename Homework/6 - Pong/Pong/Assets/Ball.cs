33602c7419ed5262a33947c086197fb4e8a78883
Angel Lim <brian.q.lim@gmail.com>
2019-02-08 23:55:14 -0800

    meme

---
+++ Homework/6 - Pong/Pong/Assets/Ball.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ball : MonoBehaviour
+{
+    public Text scoreboard1;
+    public Text scoreboard2;
+    public float speed = 5f;
+    private int score1 = 0;
+    private int score2 = 0;
+    public float sx = -0.09f;
+    public float sy = 1.23f;
+    public GameObject pong = null;
+    public GameObject triggerL = null;
+    public GameObject triggerR = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float sx = Random.Range(0, 2) == 0 ? -1 : 1;
+        float sy = Random.Range(0, 2) == 0 ? -1 : 1;
+        GetComponent<Rigidbody>().velocity = new Vector3(speed * sx, speed * sy, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pong.transform.position.x >= -6.35f)
+        {
+            score1++;
+        }
+        if (pong.transform.position.x <= 6.7f)
+        {
+            score2++;
+        }
+
+        scoreboard1.text = score1.ToString();
+        scoreboard2.text = score2.ToString();
+        
+        /*float sx = Random.Range(0, 2) == 0 ? -1 : 1;
+        float sy = Random.Range(0, 2) == 0 ? -1 : 1;
+        float botBound = -2.25f;
+        float topBound = 4.10f;
+        var pos = transform.position;*/
+
+        /*if (pos.y > topBound)
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(speed * sy, 0f);
+        }
+        else if (pos.y < botBound)
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(speed * sy, 0f);
+        }
+        transform.position = pos;*/
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other == triggerL)
+        {
+            score1++;
+        }
+
+        if(other == triggerR)
+        {
+            score2++;
+        }
+
+        scoreboard1.text = score1.ToString();
+        scoreboard2.text = score2.ToString();
+
+        var pos = pong.transform.position;
+        pos.y = sy;
+        pos.x = sx;
+        print("Score!");
+    }
+
+}
---
+++ Homework/6 - Pong/Pong/Assets/PlayerController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public KeyCode upInput = KeyCode.UpArrow;
+    public KeyCode downInput = KeyCode.DownArrow;
+    public float speed = 10.0f;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var vel = rb.velocity;
+
+        //uses rigid body components in order to create paddle movement
+        if (Input.GetKeyDown(upInput))
+        {
+            transform.Translate(Vector3.up);
+        }
+        if (Input.GetKeyDown(downInput))
+        {
+            transform.Translate(Vector3.down);
+        }
+
+        float botBound = -2.25f;
+        float topBound = 4.10f;
+        var pos = transform.position;
+
+        if (pos.y > topBound)
+        {
+            pos.y = topBound;
+        }
+        else if (pos.y < botBound)
+        {
+            pos.y = botBound;
+        }
+        transform.position = pos;
+    }
+}
+
+/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public KeyCode upInput = KeyCode.UpArrow;
+    public KeyCode downInput = KeyCode.DownArrow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(upInput))
+        {
+            transform.Translate(Vector3.up);
+        }
+        if (Input.GetKeyDown(downInput))
+        {
+            transform.Translate(Vector3.down);
+        }
+    }
+}*/
+
